d86c7aeb36f2355d16f98d053c96a4b185d3307c
ejyang <trustnloveu@gmail.com>
2023-05-22 14:52:57 +0900

    최초 커밋(기본 유틸코드 작성)

---
+++ src/main/java/module_file_util/config/CommonConsts.java
@@ -0,0 +1,19 @@
+package module_file_util.config;
+
+public interface CommonConsts {
+
+    // 파일 관련 설정값
+    public static final long FILE_MAX_SIZE          = 50 * 1024 * 1024 + 100;   // 파일 최대 사이즈 ( 50MB + 100 byte )
+    public static final long FILE_MIN_SIZE          = 1024 + 100;               // 파일 최소 사이즈 ( 1KB + 100 byte )
+    public static final long FILE_MAX_LENGTH        = 80;                       // 파일명 최대 길이
+
+    // 디렉토리 타입
+    public static final String PUBLIC       = "public";
+    public static final String PRIVATE      = "private";
+    public static final String STATIC       = "static";
+    public static final String[] DIR_TYPES  = { PUBLIC, PRIVATE, STATIC };
+
+    // 허용 확장자 타입
+    public static final String[] ALLOW_EXT_TYPES    = { "jpg", "jpeg", "png", "pdf", "xlsx", "xls", "hwp", "hwpx" };
+
+}
---
+++ src/main/java/module_file_util/util/FileCommonUtil.java
@@ -0,0 +1,89 @@
+package module_file_util.util;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static module_file_util.config.CommonConsts.*;
+
+@Component("CommonUtil")
+public class FileCommonUtil {
+
+    // #########################################################################################
+    //                                      [ PUBLIC ]
+    // #########################################################################################
+
+    /*******************************************************************************************
+     * 파일 검증 ( Null, 파일명, 용량, 확장자 )
+     *******************************************************************************************/
+    public boolean isValidFile(MultipartFile file) {
+        return !Objects.isNull(file) && isValidLength(file) && isValidSize(file) && isValidLength(file) && isValidExtType(file);
+    }
+
+
+    /*******************************************************************************************
+     * 디렉토리 타입 검증 ( public, private, static )
+     *******************************************************************************************/
+    public boolean isValidDirType(String dirType) {
+        return Arrays.asList(DIR_TYPES).contains(dirType);
+    }
+
+
+    /*******************************************************************************************
+     * 파일명 변환 - 타임스탬프 + 랜덤 정수 ( convertFileName )
+     *******************************************************************************************/
+    public String convertFileName(MultipartFile file) {
+
+        String originalName = file.getOriginalFilename();                           // 실제 파일명
+        String originalNameExtension = FilenameUtils.getExtension(originalName);    // 실제 파일명 확장자
+
+        // 파일명 변환 (타임스템프)
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddHHmmssSSS");
+        String formatDateTime = now.format(formatter);
+        int ranInt = (int) (Math.random() * 10000); // 랜덤 정수
+
+        // 최종 파일명
+        return ranInt + formatDateTime + "." + Objects.requireNonNull(originalNameExtension).toLowerCase();
+    }
+
+
+    // #########################################################################################
+    //                                      [ PRIVATE ]
+    // #########################################################################################
+
+
+    /*******************************************************************************************
+     * 파일명 길이 확인 ( isValidLength )
+     *******************************************************************************************/
+    private boolean isValidLength(MultipartFile file) {
+        return Objects.requireNonNull(file.getOriginalFilename()).length() <= FILE_MAX_LENGTH;
+    }
+
+
+    /*******************************************************************************************
+     * 파일 용량 확인 ( isValidLength ) - 최대/최소 사이즈 체크
+     *******************************************************************************************/
+    private boolean isValidSize(MultipartFile file) {
+        return file.getSize() <= FILE_MAX_SIZE && file.getSize() >= FILE_MIN_SIZE;
+    }
+
+
+    /*******************************************************************************************
+     * 파일 확장자 확인 ( isValidExtType )
+     *******************************************************************************************/
+    private boolean isValidExtType(MultipartFile file) {
+
+        String originalName = file.getOriginalFilename();                           // 실제 파일명
+        String originalNameExtension = FilenameUtils.getExtension(originalName);    // 실제 파일명 확장자
+
+        // 허용 확장자 검증
+        return Arrays.stream(ALLOW_EXT_TYPES).anyMatch(s -> s.equalsIgnoreCase(originalNameExtension));
+    }
+
+}
